b0101196d88348ab7364cb85ca558fa427f67a45
danghung <hungdang02042003@gmail.com>
2023-08-13 15:53:43 +0700

    [feat/cloudinary]: upload image

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { CloudinaryService } from './cloudinary.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('cloudinary')
-export class CloudinaryController {}
+@ApiTags('Cloudinary')
+@Controller('api/cloudinary')
+export class CloudinaryController {
+  constructor(private cloudinaryService: CloudinaryService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: any) {
+    const result = await this.cloudinaryService.uploadFile(file);
+    console.log(
+      '🚀 ~ file: cloudinary.controller.ts:20 ~ CloudinaryController ~ uploadFile ~ result:',
+      result,
+    );
+    return { url: result.secure_url, public_id: result.public_id };
+  }
+}
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,36 @@
+import * as multer from 'multer';
+
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  public upload: any;
+  private storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'quizizz',
+      format: 'png',
+    } as { folder: string; format: string },
+  });
+  constructor(private configService: ConfigService) {
+    cloudinary.config({
+      cloud_name: 'dcwdrvxdg',
+      api_key: '558211356134342',
+      api_secret: 'udY2HfWNeQFfU0SbZ5sU6g8CN9M',
+    });
+    this.upload = multer({ storage: this.storage });
+  }
+
+  async uploadFile(file: any) {
+    console.log(
+      '🚀 ~ file: cloudinary.service.ts:28 ~ CloudinaryService ~ uploadFile ~ file:',
+      file,
+    );
+    const files = file;
+    const result = await cloudinary.uploader.upload(files.path);
+    return result;
+  }
+}
